5b035f0fcac76f7a1cce34c38a9bd34153ef4f97
bhbraverman <56482025+bhbraverman@users.noreply.github.com>
2019-11-06 11:57:16 -0500

    Add files via upload

---
+++ gcloud version/app/routes.py
@@ -0,0 +1,147 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+
+#########################################
+#####       My Stuff Below          #####
+#########################################
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html', title = 'Test')
+
+@app.route('/processerror')
+def processeserror():
+    return render_template('processerror.html', title='Error')
+
+@app.route('/speciesunknown')
+def speciesunknown():
+    return render_template('speciesunknown.html', title='Mysterious')
+
+#@app.route('/newpage')
+#def newpage():
+#    return render_template('newpage.html', title='Playground')
+
+
+@app.route('/results')
+def results():
+    backend_value = 'complete'  # incomplete until function 2 finishes.
+    species = 'test'   # unknown vs. species vs. null to be filled by lookup from csv
+
+    if backend_value == 'incomplete':
+        return render_template('loading.html', title='loading') ##HOW TO SET REFRESH TIIMER
+    elif backend_value == 'complete':
+        if species =='unknown':
+            return render_template('speciesunknown.html', title='Mysterious')
+        else:
+            return render_template('results.html', title='Mysterious')
+    else:
+        return render_template('processerror.html', title='Error') 
+    
+############################
+        
+@app.route('/newresults')
+def new_results():
+    import pandas
+    df = pandas.read_csv('/Users/braverman/Desktop/query_results.csv')
+    return render_template('new_results.html', title='New Results', UniqueName = df['UniqueName'][0], GenusName = df['GenusName'][0], SpeciesName = df['SpeciesName'][0], Family = df['Family'][0], Order = df['Order'][0], CommonName = df['CommonName'][0], Leaf_Type = df['Leaf_Type'][0], GrowthRate = df['GrowthRate'][0], MatureHeight_ft = df['MatureHeight_ft'][0], URLLink = df['URLLink'][0], ImageLink = df['ImageLink'][0])
+    
+    
+############################
+
+
+
+
+
+
+
+
+
+    
+    
+#        if species == 'unknown':
+#            return render_template('test.html', title='Mysterious')
+#        else:
+#            return render_template('index.html', title='wtf?') 
+#    else:
+#        return render_template('processerror.html', title='Error')
+        
+
+#@app.route('/results')
+#def results():
+#    backend_state = 1 ### We'll make a function that tests backend to see if ready, broken, etc.
+#    species = 'spot'
+
+#    if backend_state == 1 and species == 'unknown':
+#        return render_template('speciesunknown.html', title='Mysterious')
+    
+#    elif backend_state == 1:
+#        return render_template('results.html', title='Classification Results')
+    
+#    elif backend_state == 0:
+#        return render_template('processerror.html', title='Error')
+    
+#    else:
+#        return render_template('processerror.html', title='Error')
+
+##    For above, we'll need function that keeps checking to see output form backend.
+
+    
+
+#@app.route('/prediction')
+#def prediction():
+#   if output file is a species name
+#       render everything in the output file
+#   elif output file is unknown_species
+#       render "STUMPED" template (function ran but species not known)
+#   elif error in returning file:
+#       if counter < threshold
+#           render loading page#
+#           add to counter
+#           refresh in 10 seconds
+#       if counter >= threshold:
+#           render error page (something went wrong, function did not produce output).
+#
+
+#try:
+#    f = open(fname, 'rb')
+#except OSError:
+#    print "Could not open/read file:", fname
+#    sys.exit()
+#
+#with f:
+#    reader = csv.reader(f)
+#    for row in reader:
+#        pass #do stuff here
+#
+
+
+
+
+
+
+
+
+##########   OLD INDEX BELOW:
+
+#def index():
+#    user = {'username': 'Albus'}
+#    posts = [
+#        {
+#            'author': {'username': 'Minerva'},
+#            'body': 'Disloyalty!?'
+#        },
+#        {
+#            'author': {'username': 'Severus'},
+#            'body': 'I miss Lilly'
+#        }
+#    ]
+#    return render_template('index.html', title='Home', user=user['username'], posts=posts)
+
---
+++ gcloud version/env_variables.flaskenv
@@ -0,0 +1,1 @@
+FLASK_APP=tree_main.py
